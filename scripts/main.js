d10d85860fda8dc92f739b5531b7d2d8e0a09867
conorbeamish <conorbeamish@hotmail.co.uk>
2019-10-03 16:19:11 +0100

    Redesign of landing page

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("dropdown-toggle").addEventListener("click", function(){
-    let dropdownList = document.getElementById("dropdown-list")
-    if(dropdownList.classList.contains("dropdown-show")){
-        dropdownList.classList.replace("dropdown-show", "dropdown-hide");
-    } else {
-        dropdownList.classList.replace("dropdown-hide", "dropdown-show");
-    }
-});
